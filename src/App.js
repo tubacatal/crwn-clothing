a0b1d0dea36c3b08c3989db41161b7467d40d8ea
tubacatal <tubacatal>
2020-05-01 15:03:33 +0300

    homepage component template with sass styles and sass dependency are added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import HomePage from "./homepage.component";
 import './App.css';
 
 function App() {
+
   return (
-    <div>Hello World!</div>
+    <div>
+      <HomePage/>
+    </div>
   );
 }
 
