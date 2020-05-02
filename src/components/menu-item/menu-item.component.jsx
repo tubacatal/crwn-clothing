ba3ea7a0575ab570422ffe89118f9184f6066cb6
tubacatal <tubacatal>
2020-05-02 14:44:19 +0300

    finished styling of menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,7 +5,8 @@ import './menu-item.styles.scss';
 const MenuItem = ({title, imageUrl, size}) => {
 
 	return (
-		<div style={{backgroundImage: `url(${imageUrl})`}} className={`menu-item ${size}`}>
+		<div className={`menu-item ${size}`}>
+			<div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}/>
 			<div className='content'>
 				<h1 className='title'>{title.toUpperCase()}</h1>
 				<span className='title'>SHOP NOW</span>
