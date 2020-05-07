62d46828e410fff83138dd413d6d3ef349893afc
tubacatal <tubacatal>
2020-05-07 11:14:21 +0300

    image files are internally added to the project

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,32 +11,32 @@ class Directory extends React.Component {
 			sections: [
 				{
 					title: 'hats',
-					imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+					imageUrl: require('../../images/hats.png'),
 					id: 1,
 					linkUrl: 'shop/hats'
 				},
 				{
 					title: 'jackets',
-					imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+					imageUrl: require('../../images/jackets.png'),
 					id: 2,
 					linkUrl: 'shop/jackets'
 				},
 				{
 					title: 'sneakers',
-					imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+					imageUrl: require('../../images/sneakers.png'),
 					id: 3,
 					linkUrl: 'shop/sneakers'
 				},
 				{
 					title: 'womens',
-					imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+					imageUrl: require('../../images/womens.png'),
 					size: 'large',
 					id: 4,
 					linkUrl: 'shop/womens'
 				},
 				{
 					title: 'mens',
-					imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+					imageUrl: require('../../images/men.png'),
 					size: 'large',
 					id: 5,
 					linkUrl: 'shop/mens'
