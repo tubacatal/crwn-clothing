47ebe299772b778b9bdb75d6784fc458ef9e17c7
tubacatal <tubacatal>
2020-05-07 16:05:42 +0300

    shop page and collection-preview components are created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 import './App.css';
 
@@ -10,6 +12,7 @@ const App = () => {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
