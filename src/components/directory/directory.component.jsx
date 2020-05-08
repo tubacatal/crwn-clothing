66068bcb577f5e35ae06c9f6ec2e925a7f9e4a49
tubacatal <tubacatal>
2020-05-08 16:57:48 +0300

    directory sections data is extracted into another file

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import SECTIONS_DATA from "./directory.data";
+
 import MenuItem from "../menu-item/menu-item.component";
 
 import './directory.styles.scss';
@@ -8,40 +10,7 @@ class Directory extends React.Component {
 		super();
 
 		this.state = {
-			sections: [
-				{
-					title: 'hats',
-					imageUrl: require('../../images/hats.png'),
-					id: 1,
-					linkUrl: 'shop/hats'
-				},
-				{
-					title: 'jackets',
-					imageUrl: require('../../images/jackets.png'),
-					id: 2,
-					linkUrl: 'shop/jackets'
-				},
-				{
-					title: 'sneakers',
-					imageUrl: require('../../images/sneakers.png'),
-					id: 3,
-					linkUrl: 'shop/sneakers'
-				},
-				{
-					title: 'womens',
-					imageUrl: require('../../images/womens.png'),
-					size: 'large',
-					id: 4,
-					linkUrl: 'shop/womens'
-				},
-				{
-					title: 'mens',
-					imageUrl: require('../../images/men.png'),
-					size: 'large',
-					id: 5,
-					linkUrl: 'shop/mens'
-				}
-			]
+			sections: SECTIONS_DATA
 		}
 	}
 
---
+++ src/components/directory/directory.data.js
@@ -0,0 +1,36 @@
+const SECTIONS_DATA = [
+	{
+		title: 'hats',
+		imageUrl: require('../../images/hats.png'),
+		id: 1,
+		linkUrl: 'shop/hats'
+	},
+	{
+		title: 'jackets',
+		imageUrl: require('../../images/jackets.png'),
+		id: 2,
+		linkUrl: 'shop/jackets'
+	},
+	{
+		title: 'sneakers',
+		imageUrl: require('../../images/sneakers.png'),
+		id: 3,
+		linkUrl: 'shop/sneakers'
+	},
+	{
+		title: 'womens',
+		imageUrl: require('../../images/womens.png'),
+		size: 'large',
+		id: 4,
+		linkUrl: 'shop/womens'
+	},
+	{
+		title: 'mens',
+		imageUrl: require('../../images/men.png'),
+		size: 'large',
+		id: 5,
+		linkUrl: 'shop/mens'
+	}
+];
+
+export default SECTIONS_DATA;
