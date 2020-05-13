092e7b3da0df055569ad9920cb8db4f555f95626
tubacatal <tubacatal>
2020-05-13 10:34:28 +0300

    collection preview styles and collection item components are added

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import CollectionItem from "../collection-item/collection-item.component";
+
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({title, items}) => {
@@ -10,8 +12,8 @@ const CollectionPreview = ({title, items}) => {
 			<div className='preview'>
 				{
 					items.filter((item, index) => index < 4)
-						.map((item) => {
-							return <div key={item.id}>{item.name}</div>
+						.map(({id, ...otherItemProps}) => {
+							return <CollectionItem key={id} {...otherItemProps}/>
 						})
 				}
 			</div>
