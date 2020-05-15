80e16e6003cc0658497060abc10ae47dee48b123
tubacatal <tubacatal>
2020-05-15 10:45:35 +0300

    sign-in-and-sign-up page and sign-in component creation without styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 import Header from "./components/header/header.component";
 
 import './App.css';
@@ -15,6 +16,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/sign-in' component={SignInAndSignUp}/>
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import './sign-in-and-sign-up.scss';
+
+const SignInAndSignUp = () => (
+	<SignIn/>
+);
+
+export  default SignInAndSignUp;
